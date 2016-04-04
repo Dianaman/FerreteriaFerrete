fb0ce55e1fecd66cbd01f7c229baf4143dd7475e
Diana <diana.man.91@gmail.com>
2016-04-04 14:45:53 -0300

    links y layout

---
+++ facturacion.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Ferreteria Ferrete: Facturación</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+	<link href="css/stylesheet.css" rel="stylesheet">
+</head>
+<body>
+	<h1>Ferretería Ferrete</h1>
+	<h2>Departamento de Facturación</h2>
+	<form>
+		<a href="facturacion.php" class="button">Departamento de facturación</a>
+		<a href="/construccion.php" class="button">Departamento de construcción</a>
+		<a href="/pinturas.php" class="button">Departamento de pinturas</a>
+		<a href="/iluminacion.php" class="button">Departamento de iluminación</a>
+	</form>
+</body>
+</html>
