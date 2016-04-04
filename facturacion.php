4c421996fc6bcd5ebea556d58ea44d53bc5d4a87
Diana <diana.man.91@gmail.com>
2016-04-04 15:18:11 -0300

    facturacion: layout

--- facturacion.php
+++ facturacion.php
@@ -8,11 +8,21 @@
 <body>
 	<h1>Ferretería Ferrete</h1>
 	<h2>Departamento de Facturación</h2>
-	<form>
-		<a href="facturacion.php" class="button">Departamento de facturación</a>
-		<a href="/construccion.php" class="button">Departamento de construcción</a>
-		<a href="/pinturas.php" class="button">Departamento de pinturas</a>
-		<a href="/iluminacion.php" class="button">Departamento de iluminación</a>
+	<form method="POST">
+		<label>Ingrese un primer valor: 
+		<input type="text" name="num1"></input></label>
+		<label>Ingrese un segundo valor:
+		<input type="text" name="num2"></input></label>
+		<label>Ingrese un tercer valor:
+		<input type="text" name="num3"></input></label>
+		<br><br>
+		<input type="submit" value="Sumar" class="button"></input>
+		<input type="submit" value="Promediar" class="button"></input>
+		<input type="submit" value="Final" class="button"></input>
 	</form>
+
+	<?php
+		var_dump($_POST);
+	?>
 </body>
 </html>
